b1073b41518fa15f2693cb8a6ea1184330722056
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-06-07 14:18:16 +0530

    change title
    - changed title to  '॥ श्री विष्णु सहस्रनाम ॥'

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title:  '॥ श्री विष्णु सहस्रनाम ॥',
       theme: ThemeData(
         // This is the theme of your application.
         //
