f9b2ff98a0d21e6030ef8ef4a9eed5accfd8c1f2
Preveen P <31464911+preveen-stack@users.noreply.github.com>
2023-06-07 15:52:04 +0530

    add interactivity (#15)
    - on startup first verse will be displayed
    - pressing on the button would display next verse
    - when pressed on the last verse, first verse will be displayed

--- lib/main.dart
+++ lib/main.dart
@@ -176,6 +176,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
       _counter++;
+      if (_counter == 108) _counter = 0;
     });
   }
 
